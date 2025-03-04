00783acbb94151e8dccbe93fd5975b4aa6ba7dee
estelucero <este.lucero2212@gmail.com>
2025-03-04 14:43:43 -0300

    add calculadora

---
+++ src/components/Calculadora.jsx
@@ -0,0 +1,15 @@
+
+import "../styles/calculadora.css"
+
+function Calculadora(){
+
+    return(
+        <div className="contenedor-calculadora">
+
+        </div>
+    )
+
+
+}
+
+export default Calculadora
